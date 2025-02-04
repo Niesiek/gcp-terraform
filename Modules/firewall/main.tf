43dc80390215480aaecde20435c839f9f92dd9a0
kacperproczka <kacper.proczka@o2.pl>
2025-02-04 13:47:13 +0100

    Firewall rules

---
+++ Modules/firewall/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "6.13.0"
+    }
+  }
+}
+resource "google_compute_firewall" "allow_rules" {
+  for_each = var.allow_firewall_rules
+  project = var.project
+  name = "${var.netwrok_name}-${each.key}"
+  network = var.netwrok_name
+  description = each.value.description
+  priority = each.value.priority
+  target_tags = lookup(each.value, "tags", null)
+  allow {
+    protocol = each.value.protocol
+    ports = lookup(each.value, "ports", null)
+  }
+  source_ranges = try(each.value.source_ip_ranges, [])
+}
